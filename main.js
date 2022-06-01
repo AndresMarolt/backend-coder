69e68aeba2a6396c2dc301aa9c945b969b2fedd5
AndresMarolt <andresmmarolt@gmail.com>
2022-05-31 22:38:36 -0300

    Desafio

--- main.js
+++ main.js
@@ -1,34 +1,86 @@
-class Usuario {
-    constructor(nombre, apellido, libros, mascotas) {
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.libros = [libros],
-        this.mascotas = [...mascotas]
+const { log } = require('console');
+const fs = require('fs');
+
+let id = 0;
+
+const objeto = {
+    title: "Camiseta NBA Evan Mobley",
+    price: 9500,
+    stock: 5
+}
+
+const objeto2 = {
+    title: "Camiseta NBA Jayson Tatum",
+    price: 9500,
+    stock: 5
+}
+
+class Contenedor {
+
+    constructor(fileName) {
+        this.file = fileName;
     }
 
-    getFullName() {
-        return `${this.nombre} ${this.apellido}`;
+    async save(obj) {
+        try {
+            const products = await JSON.parse(fs.promises.readFile(`./${this.file}`, 'utf-8'));
+            obj.id = id;
+            const allProducts = [...products];
+            allProducts.push(obj)
+            allProducts.push
+            id++;
+            await fs.promises.appendFile(`./${this.file}`, allProducts + "\n");
+        } catch(error) {
+            console.log("Error al guardar producto");
+            console.log(error);
+        }
+        return id;
     }
 
-    addMascota(nuevaMascota) {
-        typeof nuevaMascota == "object" ?
-            this.mascotas.push(...nuevaMascota)
-            :
-            this.mascotas.push(nuevaMascota);
+    getById(id) {
+
     }
 
-    countMascotas() {
-        return this.mascotas.length;
+    async getAll() {
+        try {
+            const allProducts = await fs.promises.readFile(`./${this.file}`, 'utf-8');
+            return allProducts;
+
+        } catch(error) {
+            console.log("Error al intentar obtener todos los productos");
+            console.log(error);
+        }
     }
 
-    addBook(nombre, autor) {
-        const nuevoLibro = {nombre: nombre, autor: autor};
-        this.libros.push(nuevoLibro);
+    async deleteById(id) {
+        try {
+            let allProducts = await fs.promises.readFile(`./${this.file}`, 'utf-8');
+            console.log(allProducts);
+            allProducts = await JSON.parse(allProducts);
+
+            console.log(allProducts);
+
+            allProducts = allProducts.filter(prod => prod.id != id);
+
+            allProducts = JSON.stringify(allProducts);
+
+            await fs.promises.writeFile(`./${this.file}`, allProducts);
+
+        } catch(error) {
+            console.log(error);
+        }
     }
 
-    getBookNames() {
-        const bookNames = this.libros.map(libro => libro.nombre)
-
-        return bookNames;
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(`./${this.file}`, "");
+        } catch(error) {
+            console.log(error);
+        }
     }
 }
+
+const file = new Contenedor("productos.txt");
+
+
+file.save(objeto);
