65c16cbcae2dd3e91682d446c28b39d2e2591154
AndresMarolt <andresmmarolt@gmail.com>
2022-06-07 15:42:42 -0300

    clase 7

--- main.js
+++ main.js
@@ -1,51 +1,79 @@
-const fs = require('fs');
-const express = require('express');
 const { log } = require('console');
-const app = express();
+
 const port = 8080;
+const express = require('express');
+const app = express();
 
-class Contenedor {
-
-    constructor(fileName) {
-        this.file = fileName;
+const productos = [
+    {
+        "title": "Escuadra",
+        "price": 123.45,
+        "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/3/3c/Squadra_45.jpg",
+        "id": 1
+    },
+    {
+        "title": "Calculadora",
+        "price": 234.56,
+        "thumbnail": "https://i.blogs.es/362ab0/calc/1366_2000.jpg",
+        "id": 2
+    },
+    {
+        "title": "Globo terráqueo",
+        "price": 345.67,
+        "thumbnail": "https://http2.mlstatic.com/D_NQ_NP_987640-MLA31076893570_062019-O.jpg",
+        "id": 3
     }
+]
 
-    async getById(id) {
-        try {
-            const productsString = await fs.promises.readFile(`./${this.file}`, 'utf-8');
-            const productsObj = JSON.parse(productsString);
-            const searchedProd = productsObj.find(product => { return product.id == id });
-            return searchedProd;
-        } catch(error) {
-            console.log("Hubo un error al buscar el id ingresado");
-            console.log(error);
-        }
-    }
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-    async getAll() {
-        try {
-            const allProducts = await fs.promises.readFile(`./${this.file}`, 'utf-8');
-            console.log(allProducts);
-            return JSON.parse(allProducts);
-
-        } catch(error) {
-            console.log("Error al intentar obtener todos los productos");
-            console.log(error);
-        }
-    }
-}
-
-const file = new Contenedor("productos.txt");
-
-app.get('/productos', (req, res) => {
-    file.getAll().then(r => res.send(r));
+app.get('/api/productos', (req, res) => {
+    res.json(productos);
 })
 
-app.get('/productoRandom', (req, res) => {
-    let randomNum = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-    file.getById(randomNum).then(r => res.send(r));
+app.get('/api/productos/:id', (req, res) => {
+    const id = Number(req.params.id);
+    if(isNaN(Number(id))) {
+        return res.json({ error: "El parámetro no es un número" });
+    }
+    
+    const filteredProd = productos.filter(producto => {
+        return producto.id === Number(id);
+    })
+
+    if(!filteredProd.length) {
+        res.status(404).json({error: "El contenido solicitado no existe"})
+    }
+
+    res.json(filteredProd);
 })
 
-app.listen(port, () => {
-    console.log(`Escuchando a puerto ${port}`);
+app.post('/api/productos', (req, res) => {
+    const { producto } = req.body;
+    producto.id = productos[productos.length - 1].id + 1;
+    productos.push(producto);
+
+    res.sendStatus(201);
+})
+
+app.post('/api/productos/:id', (req, res) => {
+    const {title, price, thumbnail} = req.body
+    const product = productos.filter(producto => {
+        return producto.id === Number(req.params.id)
+    })
+
+    product.title = title;
+    product.price = price;
+    product.thumbnail = thumbnail;
+
+    productos.push(product);
+})
+
+app.listen(port, err => {
+    if(err) {
+        console.log(`Error al iniciar el servidor: ${err}`);
+    } else {
+        console.log(`Escuchando puerto ${port}`);
+    }
 })
